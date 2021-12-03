75d1fb4fca9eb2089e852b182104386a96034681
HwanGon <hk1069@naver.com>
2021-12-03 16:55:52 +0900

    main frame2
    .

--- Timer/main.c
+++ Timer/main.c
@@ -3,12 +3,10 @@
 #include<Windows.h>
 
 int main() {
-
 	while (1) {
-
+		//메뉴 입력
 		system("cls");
 		printf("\t=== Test Program ==\n\n");
-		//메뉴 입력 
 		int input;
 		printf("1.스톱워치\t2.타이머\t3.종료\n\n");
 		printf("원하는 메뉴를 입력 하세요 : ");
@@ -16,28 +14,38 @@ int main() {
 
 		//메뉴에 따른 제어
 		if (input > 0 && input < 4) {
-			if (input == 3) break;
-
+			if (input == 3) {
+				printf("프로그램을 종료 합니다.");
+				break;
+			}
 			switch (input)
 			{
 			case 1:
+				/*
+				* 아래 기능을 포함하여 구현 예정
+				* 시작 | 기록 | 정지 | 초기화
+				*/
 				printf("1번\n");
 				system("pause");
 				break;
 			case 2:
+				/*
+				* 시간 / 분 / 초를 이용한 타이머 카운팅 시작하게 구현 예정
+				* 최대 100시간 ? 
+				* 입력을 통해 카운팅 시간 설정 
+				* 시작 | 정지 | 초기화 
+				*/
 				printf("2번\n");
 				system("pause");
 				break;
 			}
 		}
+		//예외 처리
 		else {
 			while (getchar() != '\n') {}
 			printf("잘못된 입력입니다.");
 			system("pause");
-
 		}
-
 	}
-
 	return 0;
 }
