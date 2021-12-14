07fe981c9c9a21fec9e892feed317999bf75224f
HwanGon <hk1069@naver.com>
2021-12-14 17:56:15 +0900

    Day4
    타이머 카운팅 로직 잘못됬음

--- Timer/main.c
+++ Timer/main.c
@@ -97,40 +97,47 @@ int main() {
 				scanf_s("%d", &set_sec);
 
 				printf("아무키나 누르면 타이머가 실행됩니다.\n");
-				printf("설정 시간 %d:%d:%d", set_hour, set_min, set_sec);
+				printf("설정 시간 %d:%d:%d\n", set_hour, set_min, set_sec);
 				system("pause");
-				int sec = set_sec;
-				int min = set_min;
-				int hour = set_hour;
-				int act = 0;
+
+				//표시용 변수
+				int display_sec = set_sec;
+				int display_min = set_min;
+				int display_hour = set_hour;
+				// 시간 카운팅 시작 값
 				clock_t start_time2 = clock();
 				while (1) {
 					system("cls");
+
+					// 0초로 맞춤
 					clock_t curr_time = clock() - start_time2;
 
 					double time_double = (double)curr_time / 1000;
 					int trans;
 					int undersec;
-					
+					int sec;
+					int min;
+					int hour;
 
 					trans = (int)time_double;
-					if (act == 1){ 
-						trans = (int)time_double - set_sec; 
-						//printf("작동:");
-						//system("pause");
+					undersec = curr_time % 1000;
+					sec = trans % 60;
+					min = trans / 60 % 60;
+					hour = trans / 60 / 60;
+					
 
+
+
+					printf("\n\n\t%d:%d:%d\n\n", display_hour - hour, display_min - min, display_sec - sec);
+					if (display_sec - sec == 0) {
+						display_min--;
+						display_sec = 59 + set_sec;
+					}
+					if (display_min - min == -1) {
+						display_hour--;
+						display_min = 59 + set_min;
 					}
 
-					printf("\n\n\t%d:%d:%d\n\n", hour, min, sec - trans%60);
-					if (trans % 60 - sec == 0) {
-						min--;
-						sec = 59;
-						act = 1;
-					}
-					if (set_min == -1) {
-						hour--;
-						min = 59;
-					}
 				}
 
 			}
