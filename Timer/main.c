26d88d620e7e7bc89a4abfe46b35bd33c900c535
Gon-91 <68263497+Gon-91@users.noreply.github.com>
2021-12-15 20:39:32 +0900

    Day5

--- Timer/main.c
+++ Timer/main.c
@@ -86,53 +86,119 @@ int main() {
 				//printf("2번\n");
 
 				//타이머 설정 입력 
-				int set_total = 0;;
-				printf("시간을 입력해 주세요 : ");
-				scanf_s("%d", &input);
-				set_total = input * 60 * 60;
-				printf("분을 입력해 주세요 : ");
-				scanf_s("%d", &input);
-				set_total += input * 60;
-				printf("초를 입력해 주세요 : ");
-				scanf_s("%d", &input);
-				set_total += input;
-				printf("%d\n", set_total);
+				int set_total = 0;
+				while (1) {
+					system("cls");
+					set_total = 0;
+					printf("시간을 입력해 주세요 : ");
+					scanf_s("%d", &input);
+					system("cls");
+					set_total = input * 60 * 60;
+					printf("분을 입력해 주세요 : ");
+					scanf_s("%d", &input);
+					system("cls");
+					set_total += input * 60;
+					printf("초를 입력해 주세요 : ");
+					scanf_s("%d", &input);
+					system("cls");
+					set_total += input;
 
-				printf("아무키나 누르면 타이머가 실행됩니다.\n");
-				printf("설정 시간 %d:%d:%d\n", set_total/60/60, set_total/60%60, set_total%60);
-				system("pause");
+
+					//시간 설정 확인
+					printf("설정 시간 %d:%d:%d\n", set_total / 60 / 60, set_total / 60 % 60, set_total % 60);
+					printf("1. 시작\t2. 재설정\n");
+					printf("설정 시간을 확인 해 주세요. : ");
+					scanf_s("%d", &input);
+					if (input == 1) {
+						printf("아무키나 누르면 타이머가 실행됩니다.\n");
+						system("pause");
+
+						break;
+					}
+					else {
+						printf("아무키나 누르면 타이머가 초기설정화면으로 돌아갑니다.\n");
+						system("pause");
+					}
+				}
+
+
+
 
 
 				// 시간 카운팅 시작 값
 				clock_t start_time2 = clock();
 				while (1) {
 					system("cls");
-
-					// 0초로 맞춤
+					// 0초로 맞춤과 설정 시간 - 소모시간 출력
 					clock_t curr_time = clock() - start_time2;
-
 					double time_double = (double)curr_time / 1000;
-					int trans;
-					int undersec;
-					int sec;
-					int min;
-					int hour;
-
 					int curr_sec = (int)time_double;
-					
 					int target = set_total - curr_sec;
 					printf("\n\n\t%d:%d:%d\n\n", target / 60 / 60, target / 60 % 60, target % 60);
 
+					//키입력시 일시정지
+					if (_kbhit()) {
+						system("pause");
+						printf("1. 이어하기\t2. 재설정\t 3. 종료\n");
+						printf("설정 시간을 확인 해 주세요. : ");
+						scanf_s("%d", &input);
+						if (input == 1) {
+							printf("아무키나 누르면 타이머가 실행됩니다.\n");
+							system("pause");
+							clock_t stop_time = clock() - (curr_time + start_time2);
+							double stop_time_double = (double)stop_time / 1000;
+							int stop_time_sce = (int)stop_time_double;
+							set_total = set_total + stop_time_sce;
+							start_time2 = clock();
+						}
+						else if(input == 2){
+							while (1) {
+								system("cls");
+								set_total = 0;
+								printf("시간을 입력해 주세요 : ");
+								scanf_s("%d", &input);
+								system("cls");
+								set_total = input * 60 * 60;
+								printf("분을 입력해 주세요 : ");
+								scanf_s("%d", &input);
+								system("cls");
+								set_total += input * 60;
+								printf("초를 입력해 주세요 : ");
+								scanf_s("%d", &input);
+								system("cls");
+								set_total += input;
+
+
+								//시간 설정 확인
+								printf("설정 시간 %d:%d:%d\n", set_total / 60 / 60, set_total / 60 % 60, set_total % 60);
+								printf("1. 시작\t2. 재설정\n");
+								printf("설정 시간을 확인 해 주세요. : ");
+								scanf_s("%d", &input);
+								if (input == 1) {
+									printf("아무키나 누르면 타이머가 실행됩니다.\n");
+									system("pause");
+									start_time2 = clock();
+									break;
+								}
+								else {
+									printf("아무키나 누르면 타이머가 초기설정화면으로 돌아갑니다.\n");
+									system("pause");
+								}
+							}
+						}
+						else {
+							break;
+						}
+
+					}
+
+					//시간 소모시 탈출
 					if (target == 0) {
 						printf("설정하신 시간이 모두 소모되었습니다.\n");
 						system("pause");
+						break;
 
 					}
-					printf("%d\n", target / 60 / 60);
-					printf("%d\n", target / 60 % 60);
-					printf("%d\n", target % 60);
-
-
 				}
 
 			}
